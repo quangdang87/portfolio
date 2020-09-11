9b41a6c14da29ffbd9c208030a0b10b34b1e82c5
quangdang87 <vinhquangdang1@gmail.com>
2020-09-10 21:32:42 -0700

    Finished WorkExperiences components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,50 @@
 import React from "react";
 import "./App.css";
 
+import NavBar from "./components/layout/NavBar";
+import useSticky from "./components/UseSticky";
+import TechnicalSkills from "./components/pages/TechnicalSkills";
+import WorkExperiences from "./components/pages/WorkExperiences";
+import Projects from "./components/pages/Projects";
+import Education from "./components/pages/Education";
+import Contact from "./components/pages/Contact";
+import Greetings from "./components/Greetings";
+import {Row, Col, Container} from "react-bootstrap";
+
 function App() {
+  const {isSticky, element} = useSticky();
+
   return (
-    <div className='App'>
-      <h1>hello world</h1>
+    <div
+      id='home'
+      style={{
+        backgroundImage: "linear-gradient(#102B35, #0E3B4C )"
+      }}
+    >
+      <NavBar isSticky={isSticky} />
+
+      <Container>
+        <Row>
+          <Greetings element={element} />
+        </Row>
+        <Row style={{marginTop: "20px"}}>
+          <Col style={{paddingLeft: "0px"}}>
+            <Education />
+          </Col>
+          <Col style={{paddingRight: "0px"}}>
+            <TechnicalSkills />
+          </Col>
+        </Row>
+        <Row style={{marginTop: "20px"}}>
+          <WorkExperiences />
+        </Row>
+        <Row style={{marginTop: "20px"}}>
+          <Projects />
+        </Row>
+        <Row style={{marginTop: "20px"}}>
+          <Contact />
+        </Row>
+      </Container>
     </div>
   );
 }
