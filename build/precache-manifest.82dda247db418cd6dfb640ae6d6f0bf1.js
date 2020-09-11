76d232e41e40791799e40a375b4d10f436660a00
quangdang87 <vinhquangdang1@gmail.com>
2020-09-11 01:26:26 -0700

    built

---
+++ build/precache-manifest.82dda247db418cd6dfb640ae6d6f0bf1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3efd237b69d7e801151aa860eb99ae48",
+    "url": "/index.html"
+  },
+  {
+    "revision": "512f2cdbf7ea3376b2df",
+    "url": "/static/css/main.45424f81.chunk.css"
+  },
+  {
+    "revision": "4938fa7264eba5f179c7",
+    "url": "/static/js/2.1d34f9e5.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.1d34f9e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "512f2cdbf7ea3376b2df",
+    "url": "/static/js/main.424ca4bf.chunk.js"
+  },
+  {
+    "revision": "99a3d2817b1f43b2b164",
+    "url": "/static/js/runtime-main.99ff291e.js"
+  },
+  {
+    "revision": "62d310cb96b676a6a02e682b55409c6b",
+    "url": "/static/media/quang1.62d310cb.png"
+  }
+]);
